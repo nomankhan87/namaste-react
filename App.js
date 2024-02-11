26a735307890e5bff5a6792401debc4b22991066
Noman Khan <noman@cloudways.com>
2024-02-11 17:49:26 +0500

    episode-01

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * it is and object become html which browser understand
+ * React.createElement(Object) => HTML(Browser understand)
+ */
+const parent = React.createElement(
+    "div",
+    {id: "parent"},[
+        React.createElement("div", {id: "child"},[
+            React.createElement("h1", {},("i am h1")),
+            React.createElement("h2", {},("i am h2"))
+        ]),
+        React.createElement("div", {id: "child2"},[
+            React.createElement("h1", {},("i am h1")),
+            React.createElement("h2", {},("i am h2"))
+        ]),
+    ]
+)
+
+const heading = React.createElement(
+    "h1", 
+    {id:"head", xyz:"abc"},
+    "Hello");
+
+console.log(parent);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); 
+
+root.render(parent);
