758a519c012e183594d10d0c22c5fb7ef9ced90b
Noman Khan <noman@cloudways.com>
2024-02-26 04:30:42 +0500

    episode-03 Part-1

--- App.js
+++ App.js
@@ -1,32 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//JSX (Transpiled before reach the JS) - Parcel - Babel
+
+//JSX => babel transpile into  React.createElement => ReactElement-Js Object => HTMLElement(render)
 /**
  * 
  * it is and object become html which browser understand
  * React.createElement(Object) => HTML(Browser understand)
  */
-const parent = React.createElement(
-    "div",
-    {id: "parent"},[
-        React.createElement("div", {id: "child"},[
-            React.createElement("h1", {},("This is Heading")),
-            React.createElement("h2", {},("i am h2"))
-        ]),
-        React.createElement("div", {id: "child2"},[
-            React.createElement("h1", {},("i am h1")),
-            React.createElement("h2", {},("i am h2"))
-        ]),
-    ]
-)
 
-const heading = React.createElement(
-    "h1", 
-    {id:"head", xyz:"abc"},
-    "Hello");
+//Rendering React element with JSX
 
-console.log(parent);
+const jsxHHeading = (<h1 className="head" tabIndex="5">Test react using JSX</h1>);
+
+console.log(jsxHHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 
-root.render(parent);
+root.render(jsxHHeading);
