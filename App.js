686519e05ddc0680f0a4bbabb64ba2e23b21a7e4
Noman Khan <noman@cloudways.com>
2024-03-24 18:12:17 +0500

    episode-03 Part-4 Component composition - component and element uses inside each other or

--- App.js
+++ App.js
@@ -5,28 +5,45 @@ import ReactDOM from "react-dom/client";
 
 //JSX => babel transpile into  React.createElement => ReactElement-Js Object => HTMLElement(render)
 
-//React Element
-const heading = (
+/**
+ *
+ * it is and object become html which browser understand
+ * React.createElement(Object) => HTML(Browser understand)
+ */
+
+//Composing component
+
+const elem = <span>React Element </span>;
+
+const Title = () => (
   <h1 className="head" tabIndex="5">
+  {elem}  <br></br>
     Test react using JSX
   </h1>
 );
 
-const fn = () => true
+const value = 1000;
 
-const fn2 = () => {return true}
-
-//React Functional Component
-const HeadingComponent = () => {
-    return <h1 className="head" tabIndex="5">Test react functional component</h1>
-};
-
-const HeadingComponent2 = () => ( //to write in multiple line we use reound bracket other wise use curly or no bracket
-  <h1 className="head" tabIndex="5">
-    Test react functional component
-  </h1>
+const HeadingComponent = () => (
+  <div id="container" tabIndex="5">
+    {console.log('test value', value)}
+    {value+22}
+    <Title />
+    <Title></Title>
+    {Title()}
+    <h1 className="head" tabIndex="5">
+        Test react functional component
+    </h1>
+  </div>
 );
+
+const des = (
+  <div>Description
+    <HeadingComponent/>
+  </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(des);
