0c8bc480090043910daa6dbde2192a9b94074d70
Noman Khan <noman@cloudways.com>
2024-03-24 17:40:54 +0500

    episode-03 Part-3 React element and React component

--- App.js
+++ App.js
@@ -4,18 +4,29 @@ import ReactDOM from "react-dom/client";
 //JSX (Transpiled before reach the JS) - Parcel - Babel
 
 //JSX => babel transpile into  React.createElement => ReactElement-Js Object => HTMLElement(render)
-/**
- * 
- * it is and object become html which browser understand
- * React.createElement(Object) => HTML(Browser understand)
- */
 
-//Rendering React element with JSX
+//React Element
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Test react using JSX
+  </h1>
+);
 
-const jsxHHeading = (<h1 className="head" tabIndex="5">Test react using JSX</h1>);
+const fn = () => true
 
-console.log(jsxHHeading);
+const fn2 = () => {return true}
 
-const root = ReactDOM.createRoot(document.getElementById("root")); 
+//React Functional Component
+const HeadingComponent = () => {
+    return <h1 className="head" tabIndex="5">Test react functional component</h1>
+};
 
-root.render(jsxHHeading);
+const HeadingComponent2 = () => ( //to write in multiple line we use reound bracket other wise use curly or no bracket
+  <h1 className="head" tabIndex="5">
+    Test react functional component
+  </h1>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent />);
